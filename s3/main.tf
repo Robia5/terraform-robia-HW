2bbe299c410509f6977659c3b7e6940fca871c56
rgala6574@gmail.com <rgala6574@gmail.com>
2024-05-06 09:56:50 +0600

    s3 bucket  for website changes

--- s3/main.tf
+++ s3/main.tf
@@ -15,38 +15,53 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
+#   routing_rule {
+#     condition {
+#       key_prefix_equals = "docs/"
+#     }
+#     redirect {
+#       replace_key_prefix_with = "documents/"
+#     }
+#   }
 }
-
-resource "aws_s3_bucket_ownership_controls" "control" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "web-block" {
+resource "aws_s3_bucket_policy" "static_website_policy" {
   bucket = aws_s3_bucket.static_website_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.control,
-    aws_s3_bucket_public_access_block.web-block,
-  ]
+# resource "aws_s3_bucket_ownership_controls" "control" {
+#   bucket = aws_s3_bucket.static_website_bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
-  bucket = aws_s3_bucket.static_website_bucket.id
-  acl    = "public-read"
-}
+# resource "aws_s3_bucket_public_access_block" "web-block" {
+#   bucket = aws_s3_bucket.static_website_bucket.id
+
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+# resource "aws_s3_bucket_acl" "acl" {
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.control,
+#     aws_s3_bucket_public_access_block.web-block,
+#   ]
+
+#   bucket = aws_s3_bucket.static_website_bucket.id
+#   acl    = "public-read"
+# }
